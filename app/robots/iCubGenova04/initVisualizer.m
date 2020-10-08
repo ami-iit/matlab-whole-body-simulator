35451d3b209b4e228bcd7518abaa6238c45c1eef
Lorenzo <lor.rapetti@gmail.com>
2020-10-08 14:42:48 +0200

    Add joint torque norm visualization

--- app/robots/iCubGenova04/initVisualizer.m
+++ app/robots/iCubGenova04/initVisualizer.m
@@ -16,3 +16,36 @@ confVisualizer.aroundRobot = 1; % mt
 
 % refresh rate of the picure
 confVisualizer.tStep = Config.tStep; % here equal to the time step used in the simulink model
+
+
+confVisualizer.LinkToJointMap = { [],           ... % root_link
+                               [],       ... % torso_1
+                               [],       ... % r_hip_1
+                               [],       ... % l_hip_1
+                               [12,13,14],     ... % l_hip_2
+                               [],         ... % l_upper_leg
+                               [15],         ... % l_lower_leg
+                               [], ... % l_ankle_1
+                               [16,17],     ... % l_ankle_2
+                               [18,19,20],     ... % r_hip_2
+                               [], ... % r_upper_leg
+                               [21],     ... % r_lower_leg
+                               [],     ... % r_ankle_1
+                               [22,23], ... % r_ankle_2
+                               [], ... % torso_2
+                               [1,2,3], ... % chest
+                               [], ... % r_shoulder_1
+                               [], ... % l_shoulder_1
+                               [], ... % l_shoulder_2
+                               [4,5,6],     ... % l_shoulder_3
+                               [],     ... % l_elbow_1
+                               [7], ... % l_forearm
+                               [], ... % r_shoulder_2
+                               [8,9,10], ... % r_shoulder_3
+                               [], ... % r_elbow_1
+                               [11]};... % r_forearm
+                           
+confVisualizer.SphereConfig.saturation.max=5.0;
+confVisualizer.SphereConfig.saturation.min=0.0;
+confVisualizer.SphereConfig.radius=0.05;
+confVisualizer.SphereConfig.SphereSize = 4;
--- src/robotVisualizer/Visualizer.m
+++ src/robotVisualizer/Visualizer.m
@@ -12,9 +12,12 @@ classdef Visualizer < matlab.System
 
     % Pre-computed constants
     properties (Access = private)
-        KinDynModel, visualizer;
+        KinDynModel, visualizer, tauNorm;
         g = [0, 0, -9.81]; % gravity vector
         pov = [92.9356 22.4635]; % view vector for the visualizer
+        sphere;
+        X_sphere,Y_sphere,Z_sphere;
+        max_tau, min_tau;
     end
 
     methods (Access = protected)
@@ -24,6 +27,8 @@ classdef Visualizer < matlab.System
             if obj.config.visualizeRobot
                 % Perform one-time calculations, such as computing constants
                 obj.prepareRobot()
+                
+                obj.prepareTorques();
             end
 
         end
@@ -33,7 +38,7 @@ classdef Visualizer < matlab.System
             icon = ["Robot", "Visualizer"];
         end
 
-        function stepImpl(obj, world_H_base, base_velocity, joints_positions, joints_velocity)
+        function stepImpl(obj, world_H_base, base_velocity, joints_positions, joints_velocity, joints_torque)
             % stepImpl Specifies the algorithm to execute when you run the System object
             if obj.config.visualizeRobot
                 % take the kinematic quantities and sets the robot state
@@ -42,6 +47,8 @@ classdef Visualizer < matlab.System
                 iDynTreeWrappers.updateVisualization(obj.KinDynModel, obj.visualizer);
                 % the visualizer follows the robot
                 obj.followTheRobot();
+                
+                obj.updateTorques(joints_torque);
             end
 
         end
@@ -78,6 +85,55 @@ classdef Visualizer < matlab.System
             xlim([comPosition(1) - obj.config.aroundRobot, comPosition(1) + obj.config.aroundRobot]);
             ylim([comPosition(2) - obj.config.aroundRobot, comPosition(2) + obj.config.aroundRobot]);
             zlim([comPosition(3) - obj.config.aroundRobot, comPosition(3) + obj.config.aroundRobot]);
+        end
+        
+        function updateTorques(obj, joints_torque)
+
+            for i=1:length(obj.visualizer.linkNames)
+                if(not(isempty(obj.config.LinkToJointMap{i})))
+                    % position of the link joint
+                    pos = obj.visualizer.transforms(i).Matrix(1:3,4);
+                    
+                    % compute the torque norm and the colour
+                    obj.tauNorm(i) = norm(joints_torque(obj.config.LinkToJointMap{i}));
+                    color = [min(obj.tauNorm(i),obj.max_tau)/obj.max_tau, max(obj.max_tau-max(obj.tauNorm(i)-obj.min_tau,obj.min_tau), 0)/obj.max_tau 0];
+        
+                    set(obj.sphere{i}, 'XData', obj.X_sphere + pos(1), ...
+                                       'YData', obj.Y_sphere + pos(2), ...
+                                       'ZData', obj.Z_sphere + pos(3), ...
+                                       'FaceColor', color);
+                end
+            end
+        end
+
+        function prepareTorques(obj)
+            
+            % create the sphere
+            [obj.X_sphere,obj.Y_sphere,obj.Z_sphere] = sphere(obj.config.SphereConfig.SphereSize);
+            obj.X_sphere = obj.X_sphere * obj.config.SphereConfig.radius;
+            obj.Y_sphere = obj.Y_sphere * obj.config.SphereConfig.radius;
+            obj.Z_sphere = obj.Z_sphere * obj.config.SphereConfig.radius;
+            
+            % Initialize tau_norm, max and min
+            obj.tauNorm = zeros(length(obj.visualizer.linkNames),1);
+            obj.max_tau = obj.config.SphereConfig.saturation.max;
+            obj.min_tau = obj.config.SphereConfig.saturation.min;
+            
+            for i=1:length(obj.visualizer.linkNames)
+                if(not(isempty(obj.config.LinkToJointMap{i})))
+                    % position of the link joint
+                    pos = obj.visualizer.transforms(i).Matrix(1:3,4);
+
+                    color = [0, 0, 0];
+
+                    obj.sphere{i} = surf(obj.X_sphere + pos(1), ...
+                                         obj.Y_sphere + pos(2), ...
+                                         obj.Z_sphere + pos(3), ...
+                                         'FaceColor',color, ...
+                                         'EdgeColor','none');
+                end
+            end
+
         end
 
     end
