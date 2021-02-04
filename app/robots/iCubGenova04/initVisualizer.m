b204833a7815b02c16068652e91e1b49cfdfa22a
Nuno Guedelha <nuno.guedelha@iit.it>
2021-02-04 03:38:51 +0100

    Improve the simulation speed while using the visualizer by running it at 40ms
    - treat the visualizer as atomic unit
    - set its sample time to 40ms
    - treat the fake controller as atomic unit
    - set its sample time to 1ms

--- app/robots/iCubGenova04/initVisualizer.m
+++ app/robots/iCubGenova04/initVisualizer.m
@@ -13,4 +13,4 @@ confVisualizer.world_H_base = robot_config.initialConditions.w_H_b;
 confVisualizer.aroundRobot = 1; % mt
 
 % refresh rate of the picure
-confVisualizer.tStep = Config.tStep; % here equal to the time step used in the simulink model
+confVisualizer.tStep = 0.040; % here equal to the time step used in the simulink model
