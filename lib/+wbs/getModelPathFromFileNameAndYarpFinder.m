cee307be49b142dd023838405b567550c0726fa1
Nuno Guedelha <nuno.guedelha@iit.it>
2021-03-30 06:35:23 +0200

    Rework after review of #20
    - Use the Yarp Resource Finder instead of the system call for finding the model path

--- lib/+wbs/getModelPathFromFileNameAndYarpFinder.m
+++ lib/+wbs/getModelPathFromFileNameAndYarpFinder.m
@@ -2,7 +2,11 @@ function modelPath = getModelPathFromFileNameAndYarpFinder(robotFileName)
 %getModelInfoFromRobotCfgAndYarpFinder Returns the model path and mex path prefix
 %   This function uses the Yarp Resource Finder for finding the model file.
 
-[~,modelPathFromYarpFinder] = system(['yarp resource --find ',robotFileName]);
-modelPath = strcat(fileparts(char(eval(modelPathFromYarpFinder))),'/');
+% Instanciate the YARP resource finder
+YarpRF = yarp.ResourceFinder;
+% Get the model file
+modelPathFromYarpFinder = YarpRF.findFileByName(robotFileName);
+% Just return the parent folder path
+modelPath = strcat(fileparts(modelPathFromYarpFinder),'/');
 
 end
