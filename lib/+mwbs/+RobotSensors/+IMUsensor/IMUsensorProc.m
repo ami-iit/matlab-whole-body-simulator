8d3c9b23bb3d5c739db6988db28e6f70bdaf9f00
Nuno Guedelha <nuno.guedelha@iit.it>
2021-04-30 05:53:47 +0200

    Minor fixes in `IMUsensorProc()`
    - Reordered the measurements in the concatenated output `imuOut`.
    - Improved the function documentation.

--- lib/+mwbs/+RobotSensors/+IMUsensor/IMUsensorProc.m
+++ lib/+mwbs/+RobotSensors/+IMUsensor/IMUsensorProc.m
@@ -1,14 +1,33 @@
 classdef IMUsensorProc < matlab.System
-    % IMUsensorProc This block computes the typical outputs of an IMU from the floating base kinematics
+    % IMUsensorProc This block computes the typical outputs of an IMU from the base pose and joints kinematics.
     % 
-    % Floating base kinematics inputs:
-    % - base pose, velocity and acceleration.
+    % Assuming DoFs is the number of internal degrees of freedom of the robot:
     % 
-    % Outputs:
-    % - base angular velocity, linear acceleration, and orientation (Euler angles).
+    % Input:
+    %   - w_H_b: Base pose, 4x4 matrix representing the homogenous transformation between the the
+    %   base frame and the world frame.
+    %   - nu: Base and joints velocities: Vector of size 6 representing the linear and angular
+    %   velocity of the base frame, concatenated with a vector of size DoFs, representing the
+    %   velocity of the joints.
+    %   - nuDot: Base and joints accelerations: Vector of size 6 representing the linear and angular
+    %   acceleration of the base frame, concatenated with a vector of size DoFs, representing the
+    %   acceleration of the joints.
+    %   - Jimu: Jacobian of the IMU frame.
+    %   - JimuDotNu: Bias linear acceleration of the IMU frame with respect to the inertial (world)
+    %   frame.
     % 
-    % This template includes the minimum set of functions required
-    % to define a System object with discrete state.
+    % Output:
+    %   The sensor frame orientation, linear acceleration and angular velocity w.r.t. the inertial
+    %   (world) frame. Linear acceleration and angular velocity are expressed in the world frame.
+    %   - w_rollPitchYaw: roll, pitch, yaw Euler angles in rad.
+    %   - w_linAcc: Proper linear acceleration in m/s^2.
+    %   - w_omega: Angular velocity in rad/s.
+    %   - imuOut: A [12x1] 1-D vector concatenating the previous three outputs and a placeholder for
+    %   other measurements (e.g. magnetometer).
+    %   [roll,pitch,yaw,accx,accy,accz,omegax,omegay,omegaz,0,0,0]'.
+    %   
+    % Parameters:
+    %   - Frame name: the name of the IMU frame. It should be specified in the URDF model.
 
     % Public, tunable properties
     properties
@@ -29,7 +48,7 @@ classdef IMUsensorProc < matlab.System
             % Perform one-time calculations, such as computing constants
         end
 
-        function [imuOut,w_omega,w_linAcc,w_rollPitchYaw] = stepImpl(obj,Jimu,dotJimuNu,w_H_imu,nu,nudot)
+        function [imuOut,w_omega,w_linAcc,w_rollPitchYaw] = stepImpl(obj,Jimu,JimuDotNu,w_H_imu,nu,nudot)
             % Implement algorithm. Calculate y as a function of inputs and discrete states.
             
             % Gyroscope measurements
@@ -37,7 +56,7 @@ classdef IMUsensorProc < matlab.System
             w_omega = w_imuVel(4:6);
             
             % Accelerometer measurements
-            w_imuAcc = Jimu*nudot + dotJimuNu;
+            w_imuAcc = Jimu*nudot + JimuDotNu;
             w_linAcc = w_imuAcc(1:3);
             
             % Euler angles estimation
@@ -45,7 +64,7 @@ classdef IMUsensorProc < matlab.System
             w_rollPitchYaw = wbc.rollPitchYawFromRotation(R);
             
             % composite sensor output
-            imuOut = [w_rollPitchYaw; w_omega; w_linAcc; zeros(3,1)];
+            imuOut = [w_rollPitchYaw; w_linAcc; w_omega; zeros(3,1)];
         end
 
         function resetImpl(~)
