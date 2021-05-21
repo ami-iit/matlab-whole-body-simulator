f6b4bb3c9640f2b6209ab4260a998579c6f559f8
Giuseppe <gl.giuseppelerario@gmail.com>
2021-05-21 18:01:21 +0200

    Update the matlab visualizer to the iDyntree one

--- lib/+mwbs/+Visualizers/+robotVisualizer/RobotVisualizer.m
+++ lib/+mwbs/+Visualizers/+robotVisualizer/RobotVisualizer.m
@@ -1,92 +1,100 @@
 classdef RobotVisualizer < matlab.System
     % RobotVisualizer matlab.System handling the robot visualization.
     % go in app/robots/iCub*/initRobotVisualizer.m to change the setup config
-
+    
     properties (Nontunable)
         config
+        min_time_viz = 1/25;
     end
-
+    
     properties (DiscreteState)
-
+        
     end
-
+    
     % Pre-computed constants
     properties (Access = private)
-        KinDynModel, visualizer;
+        KinDynModel, viz;
         g = [0, 0, -9.81]; % gravity vector
-        pov = [92.9356 22.4635]; % view vector for the visualizer
     end
-
+    
     methods (Access = protected)
-
+        
         function setupImpl(obj)
-
+            
             if obj.config.visualizeRobot
                 % Perform one-time calculations, such as computing constants
                 obj.prepareRobot()
+                tic;
             end
-
+            
         end
-
+        
         function icon = getIconImpl(~)
             % Define icon for System block
             icon = ["Robot", "Visualizer"];
         end
-
+        
         function stepImpl(obj, world_H_base, base_velocity, joints_positions, joints_velocity)
             % stepImpl Specifies the algorithm to execute when you run the System object
             if obj.config.visualizeRobot
-                % take the kinematic quantities and sets the robot state
+                % take the kinematic quantities and set the robot state
                 iDynTreeWrappers.setRobotState(obj.KinDynModel, world_H_base, joints_positions, base_velocity, joints_velocity, obj.g);
-                % update the visualization accordingly
-                iDynTreeWrappers.updateVisualization(obj.KinDynModel, obj.visualizer);
-                % the visualizer follows the robot
-                obj.followTheRobot();
+                if obj.viz.run()
+                    time_interval = toc;
+                    % check the maximum fps
+                    if time_interval > obj.min_time_viz
+                        obj.updateVisualization(world_H_base, joints_positions);
+                        obj.viz.draw();
+                        tic
+                    end
+                else
+                    error('Closing visualizer.')
+                end
             end
-
-        end
-
-        function prepareRobot(obj)
-            % prepareRobot Prepares the visualization loading the information
-            % Main variable of iDyntreeWrappers used for many things including updating
-            % robot position and getting world to frame transforms
-            obj.KinDynModel = iDynTreeWrappers.loadReducedModel(obj.config.jointOrder, obj.config.robotFrames.BASE, ...
-                obj.config.modelPath, obj.config.fileName, false);
-
-            % Set initial position of the robot
-            initial_base_velocity = zeros(6, 1);
-            initial_joints_velocity = zeros(length(obj.config.joints_positions));
-            iDynTreeWrappers.setRobotState(obj.KinDynModel, obj.config.world_H_base, obj.config.joints_positions, ...
-                initial_base_velocity, initial_joints_velocity, obj.g);
-
-            % Prepare figure
-            [obj.visualizer, ~] = iDynTreeWrappers.prepareVisualization(obj.KinDynModel, obj.config.meshFilePrefix, ...
-                'color', [1, 1, 1], 'material', 'metal', 'transparency', 1, 'debug', true, 'view', obj.pov, ...
-                'groundOn', true, 'groundColor', [0.5 0.5 0.5], 'groundTransparency', 0.5);
-
-            % The size of the visualizer matlab figure
-            x0 = 300;
-            y0 = 300;
-            width = 1300;
-            height = 1300;
-            set(gcf, 'position', [x0, y0, width, height])
             
-            % Window around the robot. If config.aroundRobot is a scalar, transform it to a 3x2
-            % matrix with x, y, z min/max limits.
-            if (numel(obj.config.aroundRobot) == 1)
-                wSize = obj.config.aroundRobot;
-                obj.config.aroundRobot = repmat([-wSize wSize],[3 1]);
+        end
+        
+        function prepareRobot(obj)
+            % Prepare the robot model and the iDyntree visualization
+            obj.KinDynModel = iDynTreeWrappers.loadReducedModel(obj.config.jointOrder, 'root_link', ...
+                obj.config.modelPath, obj.config.fileName, false);
+            % instantiate iDynTree visualizer
+            obj.viz = iDynTree.Visualizer();
+            obj.viz.init();
+            % add the 'model' robot the the visualizer
+            obj.viz.addModel(obj.KinDynModel.kinDynComp.model(), 'model');
+            env = obj.viz.enviroment();
+            env.setElementVisibility('floor_grid', true);
+            env.setElementVisibility('world_frame', true);
+            obj.viz.camera().animator().enableMouseControl(true);
+            % adding lights
+            obj.viz.enviroment().addLight('sun1');
+            obj.viz.enviroment().lightViz('sun1').setType(iDynTree.DIRECTIONAL_LIGHT);
+            obj.viz.enviroment().lightViz('sun1').setDirection(iDynTree.Direction(-1, 0, 0));
+            obj.viz.enviroment().addLight('sun2');
+            obj.viz.enviroment().lightViz('sun2').setType(iDynTree.DIRECTIONAL_LIGHT);
+            obj.viz.enviroment().lightViz('sun2').setDirection(iDynTree.Direction(1, 0, 0));
+        end
+        
+        function updateVisualization(obj, world_H_base, joints_positions)
+            % Update the visualization using the incoming kinematic
+            % quantities
+            s = iDynTree.VectorDynSize(obj.KinDynModel.NDOF);
+            for k = 0:length(joints_positions)-1
+                s.setVal(k,joints_positions(k+1));
             end
+            baseRotation_iDyntree = iDynTree.Rotation();
+            baseOrigin_iDyntree   = iDynTree.Position();
+            T = iDynTree.Transform();
+            for k = 0:2
+                baseOrigin_iDyntree.setVal(k,world_H_base(k+1,4));
+                for j = 0:2
+                    baseRotation_iDyntree.setVal(k,j,world_H_base(k+1,j+1));
+                end
+            end
+            T.setRotation(baseRotation_iDyntree);
+            T.setPosition(baseOrigin_iDyntree);
+            obj.viz.modelViz('model').setPositions(T, s);
         end
-
-        function followTheRobot(obj)
-            % moves the window around the robot
-            comPosition = toMatlab(obj.KinDynModel.kinDynComp.getCenterOfMassPosition());
-            xlim(comPosition(1) + obj.config.aroundRobot(1,:));
-            ylim(comPosition(2) + obj.config.aroundRobot(2,:));
-            zlim(comPosition(3) + obj.config.aroundRobot(3,:));
-        end
-
     end
-
 end
