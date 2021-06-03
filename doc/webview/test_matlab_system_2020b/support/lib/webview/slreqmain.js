c14e1136896dc4229b4da68dd9059eb58f65623a
Nuno Guedelha <nuno.guedelha@iit.it>
2021-06-04 00:30:31 +0200

    Moved webview folder and unzipped it

---
+++ doc/webview/test_matlab_system_2020b/support/lib/webview/slreqmain.js
@@ -0,0 +1,5 @@
+/*
+ Copyright 2019 The MathWorks, Inc.
+  TODO: move this code to slrequirements components. g1963364
+*/
+require(["dojo/parser","webview/interface","dojo/ready","webview/widgets/ReqApp"],function(a,b,c){c(function(){a.parse();top.slwebview=b})});
