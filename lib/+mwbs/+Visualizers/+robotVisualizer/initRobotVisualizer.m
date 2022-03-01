6eac7bc6012dd7fb06f7df6b4f0ea18c248a6d6e
VenusPasandi <venuspasandi.66@gmail.com>
2022-03-01 16:50:41 +0100

    provide backward compatibility for using the visualizer

--- lib/+mwbs/+Visualizers/+robotVisualizer/initRobotVisualizer.m
+++ lib/+mwbs/+Visualizers/+robotVisualizer/initRobotVisualizer.m
@@ -1,3 +1,11 @@
 %% configuration for the matlab iDyntree visualizer
 
+
+% Get the model path from the Urdf file name and the YARP resource finder
+if ~isfield(confVisualizer,'UrdfFile') && isfield(confVisualizer,'fileName')
+	confVisualizer.UrdfFile = confVisualizer.fileName;
+elseif ~isfield(confVisualizer,'UrdfFile') && ~isfield(confVisualizer,'fileName')
+	error('[initRobotVisualizer]: The urdf file is not provided in "confVisualizer".');
+end
+
 [confVisualizer.modelPath,confVisualizer.fileName] = mwbs.getModelPathFromFileNameAndYarpFinder(confVisualizer.UrdfFile);
