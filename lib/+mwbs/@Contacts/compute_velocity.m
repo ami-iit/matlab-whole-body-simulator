dc5576c79948b60869f181f3d44e26ba468dcb08
VenusPasandi <venuspasandi.66@gmail.com>
2022-03-04 10:58:17 +0100

    change the impact model by considering the friciton cone in the impact

--- lib/+mwbs/@Contacts/compute_velocity.m
+++ lib/+mwbs/@Contacts/compute_velocity.m
@@ -1,8 +1,11 @@
-function [base_pose_dot, s_dot] = compute_velocity(obj, M, G, robot, base_pose_dot, s_dot, closed_chains, num_inContact_frames)
+function [base_pose_dot, s_dot] = compute_velocity(obj, M, G, base_pose_dot, s_dot, closed_chains, num_inContact_frames, contact_point)
 % compute_velocity returns the configuration velocity
 % the velocity does not change if there is no impact
 % the velocity change if there is the impact
-if size(G,1) == (num_inContact_frames*3*obj.num_vertices)
+
+num_total_vertices = num_inContact_frames * obj.num_vertices;
+
+if size(G,1) == (3 * num_total_vertices)
     J_feet = G;
     J_split_points = [];
 elseif size(G,1) > (num_inContact_frames*3*obj.num_vertices)
@@ -15,34 +18,15 @@ end
 mapVerticesNewContact = obj.is_in_contact & ~obj.was_in_contact;
 new_contact = any(mapVerticesNewContact);
 
-% If a new contact is detected we should make sure that the velocity of the vertices
-% previously in contact is zero.
-if new_contact
-    mapVerticesAtZeroVel = mapVerticesNewContact | obj.was_in_contact;
-else
-    mapVerticesAtZeroVel = mapVerticesNewContact;
-end
-
-allIndexes = 1:numel(mapVerticesAtZeroVel);
-indexesVerticesAtZeroVel = (allIndexes(mapVerticesAtZeroVel)-1)*3+1; % each vertex has 3 components
-expandedIdxesVerticesAtZeroVel = [indexesVerticesAtZeroVel;indexesVerticesAtZeroVel+1;indexesVerticesAtZeroVel+2];
-expandedIdxesVerticesAtZeroVel = expandedIdxesVerticesAtZeroVel(:);
-J = J_feet(expandedIdxesVerticesAtZeroVel,1:end);
-G = [J;J_split_points];
-
 % compute the projection in the null space of the scaled Jacobian of the vertices if a
 % new contact is detected, otherwise just resuse the input variables (base_pose_dot,
 % s_dot).
 if new_contact
-    if(closed_chains==0)
-        N = (eye(robot.NDOF + 6) - M \ (G' * (G * (M \ G') \ G)));
-    else
-        N = (eye(robot.NDOF + 6) - M \ (G' * (obj.compute_damped_psudo_inverse(G * (M \ G'),0.001) * G)));
-    end
     % the velocity after the impact is a function of the velocity before the impact
     % under the constraint that the vertex velocity is equal to zeros
-    x = N * [base_pose_dot; s_dot];
-    base_pose_dot = x(1:6);
-    s_dot = x(7:end);
+    impulsive_forces = compute_unilateral_linear_impact(obj, M, [base_pose_dot; s_dot], J_feet, J_split_points, contact_point, closed_chains, num_inContact_frames, mapVerticesNewContact);
+    nu_after_impact = [base_pose_dot; s_dot] + M \ (G' * impulsive_forces);
+    base_pose_dot = nu_after_impact(1:6);
+    s_dot = nu_after_impact(7:end);
 end
 end
